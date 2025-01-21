38f610c87021230bae6e8a8478cdf6005188850d
ogechukwujane <ogeonyejekwejane@gmail.com>
2025-01-21 15:13:20 +0100

    built all component for the blog page

---
+++ app/components/blog-card/index.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="cardWrapper">
+    <div class="imageWrap">
+      <span>CORPORATE EVENTS</span>
+      <img src="public/images/logo.svg" alt="img" class="bgImage" />
+    </div>
+    <div class="content">
+      <p class="date">08.08.2021</p>
+      <h3 class="title">Top Trends in Corporate Event Design and Engagement</h3>
+      <p class="paragraph">
+        Progressively incentivize cooperative systems through technically sound
+        functionalities. Credibly productivate seamless data with flexible
+        schemas.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.cardWrapper {
+  display: flex;
+  flex-direction: column;
+  width: calc(33% - 13px);
+  @media (max-width: 1024px) {
+    width: calc(50% - 13px);
+  }
+  @media (max-width: 500px) {
+    width: 100%;
+  }
+}
+.imageWrap {
+  width: 100%;
+  height: 17.5rem;
+  border-radius: 12px;
+  overflow: hidden;
+  position: relative;
+  background: #6c757d;
+}
+.bgImage {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+}
+span {
+  color: #ffffff;
+  font-size: 0.625rem;
+  font-weight: 700;
+  padding: 8px 10px;
+  border-radius: 8px;
+  background: #00000025;
+  backdrop-filter: blur(4px);
+  position: absolute;
+  top: 10px;
+  right: 8px;
+  z-index: 2;
+}
+.content {
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+  padding: 8px 0px;
+}
+.date {
+  font-size: 0.75rem;
+  line-height: 1.225rem;
+  font-weight: 400;
+  color: #6c757d;
+}
+.title {
+  font-size: 1rem;
+  line-height: 160%;
+  font-weight: 600;
+  color: #495057;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  line-clamp: 2;
+  -webkit-line-clamp: 2;
+  overflow: hidden;
+}
+.paragraph {
+  font-size: 0.875rem;
+  line-height: 140%;
+  font-weight: 400;
+  color: #6c757d;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  line-clamp: 3;
+  -webkit-line-clamp: 3;
+  overflow: hidden;
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,22 @@
 <template>
-  <div>THis is the home page</div>
+  <div class="container mx-auto px-4 md:px-12 xl:px-0 py-8">
+    <HeroCard />
+    <div class="grid">
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+    </div>
+    <Button />
+  </div>
 </template>
+
+<style scoped>
+.grid {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1.5rem;
+}
+</style>
