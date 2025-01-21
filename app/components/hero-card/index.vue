a8daac734c01e0b86f270c9c19ae1b01dd03d683
ogechukwujane <ogeonyejekwejane@gmail.com>
2025-01-21 16:20:40 +0100

    complete blog page and view single blog page

--- app/components/hero-card/index.vue
+++ app/components/hero-card/index.vue
@@ -32,9 +32,14 @@
   overflow: hidden;
   background: #ffffff;
   box-shadow: 6px 6px 54px 0px #0000000d;
+  cursor: pointer;
   @media (max-width: 600px) {
     flex-direction: column;
     gap: 0.375rem;
+  }
+  &:hover {
+    transform: scale(1.03);
+    animation: ease-in-out;
   }
 }
 .imageWrap {
